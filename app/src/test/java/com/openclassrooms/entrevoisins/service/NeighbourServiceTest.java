36022edc7c8f9e3df6d052a5ddfc7fa07dc4ad5e
Laurent L <60913338+Laurent-Lab@users.noreply.github.com>
2020-04-21 16:19:38 +0200

    Mise en place des tests unitaires

--- app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
+++ app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.entrevoisins.service;
 
+import android.provider.ContactsContract;
+
 import com.openclassrooms.entrevoisins.di.DI;
 import com.openclassrooms.entrevoisins.model.Neighbour;
 
@@ -13,6 +15,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test on Neighbour service
@@ -27,6 +30,7 @@ public class NeighbourServiceTest {
         service = DI.getNewInstanceApiService();
     }
 
+    //Lister, ajouter un voisin un voisin
     @Test
     public void getNeighboursWithSuccess() {
         List<Neighbour> neighbours = service.getNeighbours();
@@ -34,10 +38,40 @@ public class NeighbourServiceTest {
         assertThat(neighbours, IsIterableContainingInAnyOrder.containsInAnyOrder(expectedNeighbours.toArray()));
     }
 
+    //Supprimer un voisin
     @Test
     public void deleteNeighbourWithSuccess() {
         Neighbour neighbourToDelete = service.getNeighbours().get(0);
         service.deleteNeighbour(neighbourToDelete);
         assertFalse(service.getNeighbours().contains(neighbourToDelete));
     }
+
+    //Ajouter un voisin dans la liste de favoris
+    //Vérifier si le voisin est bien ajouté à la liste de favoris
+    @Test
+    public void addFneighbourWithSucces() {
+        Neighbour neighbourToAdd = service.getNeighbours().get(0);
+        service.addFavorisNeighbour(neighbourToAdd);
+        assertTrue(service.getFneighbours().contains(neighbourToAdd));
+    }
+
+    //Vérifier si mon voisin ne fait plus partie des favoris lorsque l'on utilise à nouveau la méthode addFavorisNeighbour
+    @Test
+    public void deleteFneighbourWitchSucces() {
+        Neighbour neighbour = service.getNeighbours().get(0);
+        service.addFavorisNeighbour(neighbour);
+        Neighbour neighbourToDelete = service.getFneighbours().get(0);
+        service.addFavorisNeighbour(neighbourToDelete);
+        assertFalse(service.getFneighbours().contains(neighbourToDelete));
+    }
+
+    //Ajouter une liste de voisins
+    @Test
+    public void getfNeighboursWithSucces(){
+        List<Neighbour> fNeighbours = service.getFneighbours();
+        List<Neighbour> fNeighboursExpected = DummyNeighbourGenerator.FAVORIS_NEIGHBOURS;
+        assertThat(fNeighbours, IsIterableContainingInAnyOrder.containsInAnyOrder(fNeighboursExpected.toArray()));
+    }
+
+
 }
